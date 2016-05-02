b952447cf49481db45b3babcff404a632976b7d1
Christopher Stone <cedstone@lithoi.org.uk>
2016-05-02 16:45:47 +0100

    finished converting command definitions to nicer system, although they still need moving to a separate file

--- interactiveUI.py
+++ interactiveUI.py
@@ -27,17 +27,6 @@ except FileNotFoundError:
     network = EntityNetwork()
     status = "Created empty network (file not found)"
 
-#class command:
-#  def __init__(self, name, invocations, modes, minargs, maxargs)
-#    self.name = name
-#    self.invocations = invocations
-#    self.modes = modes
-#    self.minargs = minargs
-#    self.maxargs = maxargs
-#  def __call__(self):
-#    outcome = "just testing"
-#    return (name + ": " + outcome)
-
 
 def print_entity_list():
     """Print a list of entitites which have one of more links to/from them"""
@@ -59,8 +48,8 @@ def print_entity_links(entity):
 def print_help():
     """Display documentation"""
     global status
-    status = "Sorry, help not implemented yet"          # FIXME
-    return 0                    # number of lines printed by this function, needed to pad vertically by the right amount
+    print("Sorry, help not implemented yet")          # FIXME
+    return 1                    # number of lines printed by this function, needed to pad vertically by the right amount
 
 
 def split_input(user_input):
@@ -102,74 +91,71 @@ command_view.names = ['view', 'vw']
 command_view.accepted_args = [1]
 command_view.applicable_modes = [Mode.links, Mode.help, Mode.list]
 
+def command_remove(arguments):
+    global current_entity
+    current_entity.unlink(arguments[0], arguments[1])
+    return "Removed link [" + arguments[0] + ": " + arguments[1] + "]"
+
+command_remove.names = ['remove', 'rm']
+command_remove.accepted_args = [2]
+command_remove.applicable_modes = [Mode.links]
+
+def command_add(arguments):
+    global current_entity
+    current_entity.link(arguments[0], arguments[1])
+    return "Added link [" + arguments[0] + ": " + arguments[1] + "]"
+
+command_add.names = ['add', 'ad']
+command_add.accepted_args = [2]
+command_add.applicable_modes = [Mode.links]
+
+def command_update(arguments):
+    global current_entity
+    current_entity.relink(arguments[0], arguments[1], arguments[2])
+    return "Updated link from [" + arguments[0] + ": " + arguments[1] + "] to [" + arguments[0] + ": " + arguments[2] + "]"
+
+command_update.names = ['update', 'ud']
+command_update.accepted_args = [3]
+command_update.applicable_modes = [Mode.links]
+
+def command_help(arguments):
+    global current_mode
+    current_mode = Mode.help
+    return "Now viewing documentation"
+
+command_help.names = ['help', 'man', '?']
+command_help.accepted_args = [0]
+command_help.applicable_modes = [Mode.links, Mode.list]
+
 #-----------------
 
 commands = (
     command_list,
     command_view,
+    command_remove,
+    command_add,
+    command_update,
+    command_help,
     command_quit
 )
 
 
-def process_command(command, arguments):                #FIXME
-    """Act on a command and list of arguments - this needs improving"""
+def process_command(command, arguments):
     global quitting, current_mode, current_entity, status       # Not sure if globals are nice or not...
     
     invocation = command
     for command in commands:
         if invocation in command.names:
             if len(arguments) not in command.accepted_args:
-                status = invocation + ": Unacceptable number of arguments"
+                status = command.names[0] + ": Unacceptable number of arguments"
                 break
             if current_mode not in command.applicable_modes:
-                status = invocation + ": Inappropriate mode"
+                status = command.names[0] + ": Inappropriate mode"
                 break
             status = command(arguments)
             break
     else:
         status = invocation + " - unknown command"
-    #for command in commands:
-    #if invocation in command.invocations:
-        #status = command()
-        #break
-    #else:
-    #status = invocation + " - unknown command"
-
-
-    #status = command + ": Operation sucessful"          # Fallback status - is assuming sucess really a good idea?
-    #try:                                                # Hacky way to catch errors - will be fixed in new code (commented out)
-    #    if command in ["quit", "exit"]:
-    #        status = "Exiting by user request..."
-    #        quitting = True
-    #    elif command in ["help", "?", "man", "manual"]:
-    #        current_mode = Mode.help
-    #        status = "Entered help mode"
-    #    elif command in ["list", "ls"]:
-    #        current_mode = Mode.list
-    #        status = "Entered entity list mode"
-    #    elif command in ["view", "vw"]:
-    #        current_mode = Mode.links
-    #        current_entity = network[arguments[0]]
-    #        status = "Now viewing entity: " + arguments[0]
-    #    elif command in ["remove", "rm"]:
-    #        if current_mode == Mode.links:
-    #            current_entity.unlink(arguments[0], arguments[1])
-    #        else:
-    #            status = "Please enter entity view mode using 'view <entity>' and try again"
-    #    elif command in ["add", "ad"]:
-    #        if current_mode == Mode.links:
-    #            current_entity.link(arguments[0], arguments[1])
-    #        else:
-    #            status = "Please enter entity view mode using 'view <entity>' and try again"
-    #    elif command in ["update", "ud"]:
-    #        if current_mode == Mode.links:
-    #            current_entity.relink(arguments[0], arguments[1], arguments[2])
-    #        else:
-    #            status = "Please enter entity view mode using 'view <entity>' and try again"
-    #    else:
-    #        status = "Error: Unknown command"
-    #except:
-    #    status = command + ": Operation failed"
 
 
 quitting = False
