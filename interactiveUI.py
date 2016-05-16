d823102212a278ba6efcb0da76385db41cec1c46
Francis Herne <francis.herne@yahoo.co.uk>
2016-05-16 01:09:57 +0100

    Port to new network stuff

--- interactiveUI.py
+++ interactiveUI.py
@@ -3,7 +3,7 @@ import shutil
 import termcolor
 
 from enum import Enum
-from network import EntityNetwork
+from network import Link, Network
 
 
 print("\x1B[22t") # Save window title
@@ -30,12 +30,11 @@ print("\x1B]0;%s\x07" % "PIMesh") # Set window title
 filename = "network0.pimesh"            # Currently fixed filename - should it really be an argument when starting the script?
 
 try:
-    network = EntityNetwork.from_file(filename)         # Note that this does not (and should not) create a new file
+    network = Network.from_file(filename)         # Note that this does not (and should not) create a new file
     UI.status = "Loaded PIMesh network from file"
 except FileNotFoundError:
-    network = EntityNetwork()
+    network = Network()
     UI.status = "Created empty network (file not found)"
-
 
 def print_entity_list():
     """Print a list of entitites which have one of more links to/from them"""
@@ -43,17 +42,17 @@ def print_entity_list():
     if len(network) == 0:
         print("(No entities in network)")           # Could be slightly confusing, but saves a big empty space
         return 1
-    for n, name in enumerate(network):
+    for n, name in enumerate(network.origins):
         print(str(n) + " | " + name)
     return len(network)         # number of lines printed by this function, needed to pad vertically by the right amount
 
 
-def print_entity_links(entity):
+def print_links(name):
     """Print a list of links associated with a specific entity"""
-    print(entity.name + ':\n')
-    for n, link in enumerate(entity.links):
-        print(str(n) + " | " + link[0] + ": " + link[1].name)
-    return len(entity.links)+2 # number of lines printed by this function, needed to pad vertically by the right amount
+    print(name + ':\n')
+    for n, link in enumerate(network[name]):
+        print(str(n) + " | " + link.tag + ": " + link.target)
+    return len(network[name])+2 # number of lines printed by this function, needed to pad vertically by the right amount
 
 
 def print_help():
@@ -98,11 +97,10 @@ command_list.applicable_modes = [Mode.links, Mode.help]
 
 def command_view(arguments):
     """View the links associated with a specified entity, creating that entity if required"""
-    global current_mode, current_entity
+    global current_mode, current_name
     current_mode = Mode.links
-    current_entity = network[arguments[0]]
-    #current_entity.link(arguments[0], arguments[1])
-    return 'Now viewing entity "' + arguments[0] + '"'
+    current_name = arguments[0]
+    return 'Now viewing entity "' + current_name + '"'
 
 command_view.names = ['view', 'vw']
 command_view.accepted_args = [1]
@@ -110,9 +108,14 @@ command_view.applicable_modes = [Mode.links, Mode.help, Mode.list]
 
 def command_remove(arguments):
     """Remove a specific link associated with the current entity"""
-    global current_entity
-    current_entity.unlink(arguments[0], arguments[1])
-    return 'Removed link "' + arguments[0] + ": " + arguments[1] + '"'
+    global current_name
+    tag, target, *rest = arguments[0], arguments[1]
+    inverse_tag = rest[0] if rest else Network.reciprocal(tag)
+    try:
+        network.unlink(current_name, tag, target, inverse_tag)
+        return 'Removed link "' + tag + ": " + target + '"'
+    except ValueError:
+        return "No such link."
 
 command_remove.names = ['remove', 'rm']
 command_remove.accepted_args = [2]
@@ -120,9 +123,14 @@ command_remove.applicable_modes = [Mode.links]
 
 def command_add(arguments):
     """Add a new link to the current entity"""
-    global current_entity
-    current_entity.link(arguments[0], arguments[1])
-    return 'Added link "' + arguments[0] + ": " + arguments[1] + '"'
+    global current_name
+    tag, target, *rest = arguments[0], arguments[1]
+    inverse_tag = rest[0] if rest else Network.reciprocal(tag)
+    try:
+        network.link(current_name, tag, target, inverse_tag)
+        return 'Added link "' + tag + ": " + target + '"'
+    except ValueError:
+        return "Link already existed."
 
 command_add.names = ['add', 'ad']
 command_add.accepted_args = [2]
@@ -130,22 +138,23 @@ command_add.applicable_modes = [Mode.links]
 
 def command_update(arguments):
     """Change the target of one of the current entity's links"""
-    global current_entity
+    global current_name
     tag = arguments[0]
-    if len(arguments) == 2:
-        if len(current_entity[tag]) > 1:
-            return 'Sorry, tag "' + tag + '" is ambiguous.'
-        if len(current_entity[tag]) < 1:
-            return 'Sorry, tag "' + tag + '" is not associated with this entity.'
-        old_target = current_entity[tag][0]
-        current_entity.relink(tag, old_target, arguments[1])
-        return 'Updated link from "' + tag + ': ' + old_target.name + '" to "' + tag + ': ' + arguments[1] + '"'
+    if (len(arguments) == 2):
+        old_target, new_target = (...), arguments[1]
     else:
-        if len(current_entity[tag]) < 1 or arguments[1] not in (target.name for target in current_entity[tag]):
-            return '"' + tag + ': ' + arguments[1] + '" - no such link for this entity'
-        current_entity.relink(tag, arguments[1], arguments[2])
-        return 'Updated link from "' + tag + ': ' + arguments[1] + '" to "' + tag + ': ' + arguments[2] + '"'
-    
+        old_target, new_target = arguments[1:]
+
+    to_replace = network[current_name, tag, old_target]
+    if not len(to_replace):
+        return '"' + tag + ': ' + old_target + '" - no such link for this entity'
+    if len(to_replace) > 1:
+        return 'Sorry, tag "' + tag + '" is ambiguous.'
+    inverse_tag = to_replace[0].inverse_tag
+    to_replace.unlink()
+    network.link(current_name, tag, new_target, inverse_tag)
+
+    return 'Updated link from "' + tag + ': ' + old_target + '" to "' + tag + ': ' + new_target + '"'
 
 command_update.names = ['update', 'ud']
 command_update.accepted_args = [2, 3]
@@ -175,7 +184,7 @@ commands = (
 
 
 def process_command(invocation, arguments):
-    global quitting, current_mode, current_entity, UI       # Not sure if globals are nice or not...
+    global quitting, current_mode, current_name, UI       # Not sure if globals are nice or not...
     
     for command in commands:
         if invocation in command.names:
@@ -189,18 +198,15 @@ def process_command(invocation, arguments):
             break
     else:
         try:
-            #int(invocation)
-            #UI.status = "Spotted a number being used as a command!"
             if current_mode == Mode.links:
-                UI.status = command_view([current_entity.links[int(invocation)][1].name,])
+                goto = network[current_name].targets[int(invocation)]
+                UI.status = command_view((goto,))
             elif current_mode == Mode.list:
-                UI.status = command_view([tuple(network.keys())[int(invocation)],])
+                UI.status = command_view((network[int(invocation)].origin,))
             else:
                 UI.status = "Numeric shortcuts don't work in this mode"
         except IndexError:
-            UI.status = invocation + " - not the index of any entity"
-        except ValueError:
-            UI.status = invocation + " - unknown command"
+            UI.status = invocation + " - invalid index"
 
 
 quitting = False
@@ -210,7 +216,7 @@ while not quitting:
     if current_mode == Mode.list:               # Elif chains are ugly - FIXME
         used_lines = print_entity_list()
     elif current_mode == Mode.links:
-        used_lines = print_entity_links(current_entity)
+        used_lines = print_links(current_name)
     elif current_mode == Mode.help:
         used_lines = print_help()
     else:
