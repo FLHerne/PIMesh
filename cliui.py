c5f2b06c2caa8f906b11e46173b8f5bb171133fd
Christopher Stone <cedstone@lithoi.org.uk>
2016-05-27 00:22:51 +0100

    added command for mode switching, modeified duck

--- cliui.py
+++ cliui.py
@@ -5,12 +5,14 @@ from enum import Enum
 
 
 class UI:
-  Mode = Enum('Mode', ('list', 'links', 'help'))
+  Mode = Enum('Mode', ('list', 'links', 'help', 'duck'))
   mode_names = {
     Mode.list: "Entity List",
     Mode.links: "Single entity view",
-    Mode.help: "Documentation"
+    Mode.help: "Documentation",
+    Mode.duck: "Anatid Display"
   }
+  
  
   def __init__(self, network):
     self.network = network
@@ -25,16 +27,44 @@ class UI:
     
     self.commands = {
       "duck": self.command_duck,
+      "list": self.command_list,
       "quit": self.command_quit,
       "exit": self.command_quit
     }
+    
+    self.mode_content = {
+      UI.Mode.list: self.list_print,
+      UI.Mode.links: self.links_print,
+      UI.Mode.help: self.help_print,
+      UI.Mode.duck: self.duck_print
+    }
       
+  def list_print(self):	
+    print("list goes here")
+    return 1
+  
+  def help_print(self):	
+    print("help goes here")
+    return 1
+  
+  def links_print(self):	
+    print("links go here")
+    return 1
+  
+  def duck_print(self):	
+    print("\n   >(')____, \n    (` =~~/  \n ~^~^`---'~^~^~")
+    return 4
       
   ###########
   def command_duck(self, mode, arguments):
-    print("\n   >(')____, \n    (` =~~/  \n ~^~^`---'~^~^~")
-    self.used_lines = 4
+    """Draw a duck"""
+    self.mode = self.Mode.duck
     return("Quack!")
+  
+  def command_list(self, mode, arguments):
+    """Draw a duck"""
+    self.mode = self.Mode.list
+    return("Now entering list mode")
   
   def command_quit(self, mode, arguments):
       """Trigger a clean exit from the program, saving changes"""
@@ -68,6 +98,7 @@ class UI:
   def run(self):
     while not self.quitting:
       self.cols, self.lines = shutil.get_terminal_size()
+      self.used_lines = self.mode_content[self.mode]()  #FIXME
       self.vertical_pad()
       self.print_status_line()
       self.command, self.arguments = self.split_input(input("> "))
