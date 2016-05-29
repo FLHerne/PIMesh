cbb3653699f29bee8c8d9299b0f444b44e81a37b
Christopher Stone <cedstone@lithoi.org.uk>
2016-05-29 21:53:38 +0100

    corrected file saving

--- pimesh.py
+++ pimesh.py
@@ -7,8 +7,9 @@ from cliui import UI
 
 try:
   filename = sys.argv[1]
+  networkfile = open(filename, 'r+')
   try:
-      network = Network.from_file(filename)         # Note that this does not (and should not) create a new file
+      network = Network.from_file(networkfile)         # Note that this does not (and should not) create a new file
       print("Loaded PIMesh network from file")
   except FileNotFoundError:
       network = Network()
@@ -22,7 +23,7 @@ try:
       print("Caught leopard interrupt")
       
   try:
-      network.to_file(filename)
+      network.to_file(networkfile)
       print("Saved session changes to file.")
   except:
       print("Saving to file failed!")
