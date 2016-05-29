5893dec5d2327720d9c9ceb8b094188ec27e23ea
Christopher Stone <cedstone@lithoi.org.uk>
2016-05-29 19:06:43 +0100

    handle missing cli arguments

--- pimesh.py
+++ pimesh.py
@@ -2,27 +2,30 @@ import sys
 from network import Network
 from cliui import UI
 
-filename = sys.argv[1]
-
 try:
-    network = Network.from_file(filename)         # Note that this does not (and should not) create a new file
-    print("Loaded PIMesh network from file")
-except FileNotFoundError:
-    network = Network()
-    print("Created empty network (file not found)")
+  filename = sys.argv[1]
+  try:
+      network = Network.from_file(filename)         # Note that this does not (and should not) create a new file
+      print("Loaded PIMesh network from file")
+  except FileNotFoundError:
+      network = Network()
+      print("Created empty network (file not found)")
 
-ui = UI(network)
+  ui = UI(network)
 
-try:
-    ui.run()
-except KeyboardInterrupt:
-    print("Caught leopard interrupt")
-    
-try:
-    network.to_file(filename)
-    print("Saved session changes to file.")
-except:
-    print("Saving to file failed!")
+  try:
+      ui.run()
+  except KeyboardInterrupt:
+      print("Caught leopard interrupt")
+      
+  try:
+      network.to_file(filename)
+      print("Saved session changes to file.")
+  except:
+      print("Saving to file failed!")
+     
+except IndexError:
+  print("Please specify filename as first argument.")
     
 print("PIMesh quitting...")
 
