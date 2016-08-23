790f180204b87690d8a69f1f18626d43c9706e3d
Francis Herne <mail@flherne.uk>
2016-08-24 00:45:56 +0100

    Add endpoint to get raw file

--- netui.py
+++ netui.py
@@ -4,27 +4,39 @@ from jinja2 import Environment, FileSystemLoader
 
 from network import Network
 
-with open('network0.pimesh') as net_file:
+filename = 'network0.pimesh'
+
+with open(filename) as net_file:
     network = Network.from_file(net_file)
 
 jinja_env = Environment(loader=FileSystemLoader('./html'),
                         extensions=['jinja2.ext.autoescape'],
-                        autoescape=False)
+                        autoescape=True)
 
 tpl_list = jinja_env.get_template('list.html')
 tpl_view = jinja_env.get_template('view.html')
 
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
 
-    output = ""
+    status = "200 OK"
+    headers = [('Content-Type','text/html')]
+    body = ""
+
     path = env.get('PATH_INFO', "").lstrip('/')
     cmd, *rest = path.split("/", 1)
     arg = unquote(rest[0]).strip() if rest else ""
 
-    print("A", cmd, "B", arg)
-    if cmd == "view" and arg:
-        output = tpl_view.render(name=arg, links=network[arg])
+    if path == "plain":
+        headers = [
+            ('Content-Type','text/plain'),
+            ('Content-Disposition', 'attachment; filename="%s"' %(filename))
+        ]
+        with open(filename) as net_file:
+            body = net_file.read()
+    elif cmd == "view" and arg:
+        body = tpl_view.render(name=arg, links=network[arg])
     else:
-        output = tpl_list.render(entities=network.origin_counts())
-    return [bytes(output, 'UTF-8')]
+        body = tpl_list.render(entities=network.origin_counts())
+
+    start_response(status, headers)
+    return [bytes(body, 'UTF-8')]
